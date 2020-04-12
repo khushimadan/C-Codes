6e4f29745808dac98fbc7e1a3e1ce3f8089e61dc
khushimadan <63074598+khushimadan@users.noreply.github.com>
2020-04-13 01:02:21 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+number = int(input("Enter a number: "))
+remainder = number % 2;
+if remainder == 0:
+  print ("{0} is even".format(number))
+else: 
+  print ("{0} is odd and remainder is {1}.".format(number, remainder))
+  
+  
