ab2f9e7be2bafa5152d986c8e7bee11dcdc56a9d
khushimadan <63074598+khushimadan@users.noreply.github.com>
2020-12-27 01:34:48 +0530

    Delete evenodd.py

--- evenodd.py
+++
@@ -1,8 +0,0 @@
-number = int(input("Enter a number: "))
-remainder = number % 2;
-if remainder == 0:
-  print ("{0} is even".format(number))
-else: 
-  print ("{0} is odd and remainder is {1}.".format(number, remainder))
-  
-  
